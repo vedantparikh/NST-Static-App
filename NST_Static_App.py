bfb2b63f938e6b69ebecc74a74efc19d7e65e754
vedantparikh <44754510+vedantparikh@users.noreply.github.com>
2020-05-29 17:14:30 +0200

    requests to add files

--- NST_Static_App.py
+++ NST_Static_App.py
@@ -33,7 +33,7 @@ taken digitally somewhere in Austria and the style image ([**Starry Night**](htt
 know how would it look like? The below image shows it would look something like this.  
 
 """
-st.image(Image.open(requests.get(BASE_DIR + "linkedin.jpg")), width=512)
+st.image(Image.open(img_dir + "linkedin.jpg"), width=512)
 
 
 def returnNotMatches(a, b):
